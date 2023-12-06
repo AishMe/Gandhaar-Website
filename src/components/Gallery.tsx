033f007ba94b65e067594a8e7efe2d5e6ba497d6
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2023-12-06 21:15:44 +0530

    All Major Functionalities Done

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,24 +1,51 @@
 // ImageHoverEffect.js
+"use client"
+import React, { useState, useEffect } from 'react';
 
-import React from 'react';
 
 const ImageHoverEffect = () => {
+
+  const [cursorPosition, setCursorPosition] = useState({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  });
+
+  useEffect(() => {
+    const updateCursorPosition = (event: any) => {
+      setCursorPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', updateCursorPosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateCursorPosition);
+    };
+  }, []);
+
   return (
-    <div className="image-hover-container">
-      {Array.from({ length: 6 }, (_, columnIndex) => (
-        <div key={columnIndex} className={`column column-${columnIndex + 1}`}>
-          <div className="box-container">
-            {Array.from({ length: 6 }, (_, boxIndex) => (
-              <div
-                key={boxIndex}
-                className={`box box-${boxIndex + 1} column-${columnIndex + 1}`}
-              >
-                {/* Box Content */}
-              </div>
-            ))}
+    <div className="relative">
+      <div
+        className="absolute h-full w-full top-0 left-0 spotlight opacity-95"
+        style={{
+          background: `radial-gradient(circle at ${cursorPosition.x}px ${cursorPosition.y}px, transparent 5%, rgb(0, 0, 0) 35vh)`,
+        }}
+      ></div>
+      <div className="image-hover-container">
+        {Array.from({ length: 6 }, (_, columnIndex) => (
+          <div key={columnIndex} className={`column column-${columnIndex + 1}`}>
+            <div className="box-container">
+              {Array.from({ length: 50 }, (_, boxIndex) => (
+                <div
+                  key={boxIndex}
+                  className={`box box-${boxIndex + 1} column-${columnIndex + 1} rounded-lg`}
+                >
+                  {/* Box Content */}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
